286db5b4e38c33f2da0587af227249d42ac91c9e
Harshil Gajjar <harshilgajjar1601@gmail.com>
2026-03-30 21:27:36 +0530

    created login page and signup page

---
+++ frontend/user_UI/user_Login/userLogin.js
@@ -0,0 +1,14 @@
+function togglePassword() {
+    const password = document.getElementById("password");
+    const icon = document.getElementById("eyeIcon");
+
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
