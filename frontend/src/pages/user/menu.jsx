b4a88912ab7b94c9cb3d19047868f698e88c3895
Harshil Gajjar <harshilgajjar1601@gmail.com>
2026-04-12 16:17:09 +0530

    all items are added

--- frontend/src/pages/user/menu.jsx
+++ frontend/src/pages/user/menu.jsx
@@ -4,16 +4,16 @@ import fries from "../../assets/Images/fries.jpg";
 import coffee from "../../assets/Images/coffee.jpeg";
 import dosa from "../../assets/Images/dosa.webp";
 import paneerPizza from "../../assets/Images/paneerPizza.jpg";  
-// import vegSandwich from "../../assets/Images/vegSandwich.jpeg";
-// import tea from "../../assets/Images/tea.jpeg";
-// import chocolateShake from "../../assets/Images/chocolateShake.jpeg";
-// import vegNoodles from "../../assets/Images/vegNoodles.webp";
-// import manchurian from "../../assets/Images/manchurian.jpeg";
-// import pavBhaji from "../../assets/Images/pavBhaji.jpeg";
-// import samosa from "../../assets/Images/samosa.jpeg";
-// import kachori from "../../assets/Images/kachori.jpeg";
-// import iceCream from "../../assets/Images/iceCream.jpeg";
-// import fruitJuice from "../../assets/Images/fruitJuice.webp";
+import vegSandwich from "../../assets/Images/vegSandwich.jpeg";
+import tea from "../../assets/Images/tea.jpeg";
+import chocolateShake from "../../assets/Images/chocolateShake.jpeg";
+import vegNoodles from "../../assets/Images/vegNoodles.webp";
+import manchurian from "../../assets/Images/manchurian.jpeg";
+import pavBhaji from "../../assets/Images/pavBhaji.jpeg";
+import samosa from "../../assets/Images/samosa.jpeg";
+import kachori from "../../assets/Images/kachori.jpeg";
+import iceCream from "../../assets/Images/iceCream.jpeg";
+import fruitJuice from "../../assets/Images/fruitJuice.webp";
 
 import "../../styles/user/menu.css";
 import Sidebar from "../../components/sidebar";
@@ -85,117 +85,117 @@ function Menu() {
           <div className="food-card">
             <img src={burger} alt="Burger" />
             <h4>Burger</h4>
-            <p>₹150</p>
-            <button onClick={() => addToCart({ id: 1, name: "Burger", price: 150 , image: burger })}>Add to Cart</button>
+            <p>₹90</p>
+            <button onClick={() => addToCart({ id: 1, name: "Burger", price: 90 , image: burger })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
             <img src={pizza} alt="Pizza" />
             <h4>Pizza</h4>
-            <p>₹120</p>
-            <button onClick={() => addToCart({ id: 2, name: "Pizza", price: 120 , image: pizza })}>Add to Cart</button>
+            <p>₹150</p>
+            <button onClick={() => addToCart({ id: 2, name: "Pizza", price: 150 , image: pizza })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
             <img src={fries} alt="French Fries" />
             <h4>French Fries</h4>
-            <p>₹90</p>
-            <button onClick={() => addToCart({ id: 3, name: "French Fries", price: 90 , image: fries })}>Add to Cart</button>
+            <p>₹120</p>
+            <button onClick={() => addToCart({ id: 3, name: "French Fries", price: 120 , image: fries })}>Add to Cart</button>
           </div>
 
           <div className="food-card drink">
             <img src={coffee} alt="Cold Coffee" />
             <h4>Cold Coffee</h4>
-            <p>₹80</p>
-            <button onClick={() => addToCart({ id: 4, name: "Cold Coffee", price: 80 , image: coffee })}>Add to Cart</button>
+            <p>₹100</p>
+            <button onClick={() => addToCart({ id: 4, name: "Cold Coffee", price: 100 , image: coffee })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
             <img src={dosa} alt="Masala Dosa" />
             <h4>Masala Dosa</h4>
-            <p>₹100</p>
-            <button onClick={() => addToCart({ id: 5, name: "Masala Dosa", price: 100 , image: dosa })}>Add to Cart</button>
+            <p>₹190</p>
+            <button onClick={() => addToCart({ id: 5, name: "Masala Dosa", price: 190 , image: dosa })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
             <img src={paneerPizza} alt="Paneer Pizza" />
             <h4>Paneer Pizza</h4>
-            <p>₹150</p>
-            <button onClick={() => addToCart({ id: 6, name: "Paneer Pizza", price: 150 , image: paneerPizza })}>Add to Cart</button>
+            <p>₹350</p>
+            <button onClick={() => addToCart({ id: 6, name: "Paneer Pizza", price: 350 , image: paneerPizza })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Veg Sandwich" />
+            <img src={vegSandwich} alt="Veg Sandwich" />
             <h4>Veg Sandwich</h4>
             <p>₹60</p>
             <button onClick={() => addToCart({ id: 7, name: "Veg Sandwich", price: 60 , image: vegSandwich })}>Add to Cart</button>
           </div>
 
           <div className="food-card drink">
-            <img src={"#"} alt="Tea" />
+            <img src={tea} alt="Tea" />
             <h4>Tea</h4>
             <p>₹20</p>
             <button onClick={() => addToCart({ id: 8, name: "Tea", price: 20 , image: tea })}>Add to Cart</button>
           </div>
 
           <div className="food-card drink">
-            <img src={"#"} alt="Coffee" />
+            <img src={coffee} alt="Coffee" />
             <h4>Coffee</h4>
-            <p>₹30</p>
-            <button onClick={() => addToCart({ id: 9, name: "Coffee", price: 30 , image: coffee })}>Add to Cart</button>
+            <p>₹40</p>
+            <button onClick={() => addToCart({ id: 9, name: "Coffee", price: 40 , image: coffee })}>Add to Cart</button>
           </div>
 
           <div className="food-card drink">
-            <img src={"#"} alt="Chocolate Shake" />
+            <img src={chocolateShake} alt="Chocolate Shake" />
             <h4>Chocolate Shake</h4>
-            <p>₹120</p>
-            <button onClick={() => addToCart({ id: 10, name: "Chocolate Shake", price: 120 , image: chocolateShake })}>Add to Cart</button>
+            <p>₹90</p>
+            <button onClick={() => addToCart({ id: 10, name: "Chocolate Shake", price: 90 , image: chocolateShake })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Veg Noodles" />
+            <img src={vegNoodles} alt="Veg Noodles" />
             <h4>Veg Noodles</h4>
-            <p>₹110</p>
-            <button onClick={() => addToCart({ id: 11, name: "Veg Noodles", price: 110 , image: vegNoodles })}>Add to Cart</button>
+            <p>₹210</p>
+            <button onClick={() => addToCart({ id: 11, name: "Veg Noodles", price: 210 , image: vegNoodles })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Manchurian" />
+            <img src={manchurian} alt="Manchurian" />
             <h4>Manchurian</h4>
-            <p>₹130</p>
-            <button onClick={() => addToCart({ id: 12, name: "Manchurian", price: 130 , image: manchurian })}>Add to Cart</button>
+            <p>₹100</p>
+            <button onClick={() => addToCart({ id: 12, name: "Manchurian", price: 100 , image: manchurian })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Pav Bhaji" />
+            <img src={pavBhaji} alt="Pav Bhaji" />
             <h4>Pav Bhaji</h4>
             <p>₹120</p>
             <button onClick={() => addToCart({ id: 13, name: "Pav Bhaji", price: 120 , image: pavBhaji })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Samosa" />
+            <img src={samosa} alt="Samosa" />
             <h4>Samosa</h4>
             <p>₹25</p>
             <button onClick={() => addToCart({ id: 14, name: "Samosa", price: 25 , image: samosa })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Kachori" />
+            <img src={kachori} alt="Kachori" />
             <h4>Kachori</h4>
             <p>₹30</p>
             <button onClick={() => addToCart({ id: 15, name: "Kachori", price: 30 , image: kachori })}>Add to Cart</button>
           </div>
 
           <div className="food-card">
-            <img src={"#"} alt="Ice Cream" />
+            <img src={iceCream} alt="Ice Cream" />
             <h4>Ice Cream</h4>
             <p>₹100</p>
             <button onClick={() => addToCart({ id: 16, name: "Ice Cream", price: 100 , image: iceCream })}  >Add to Cart</button>
           </div>
 
           <div className="food-card drink">
-            <img src={"#"} alt="Fruit Juice" />
+            <img src={fruitJuice} alt="Fruit Juice" />
             <h4>Fruit Juice</h4>
             <p>₹70</p>
             <button onClick={() => addToCart({ id: 17, name: "Fruit Juice", price: 70 , image: fruitJuice })}>Add to Cart</button>
